b229fca1e535e5727253bf3764f13d1e5cf2c8e3
Weronka Golonka <54840864+weronkagolonka@users.noreply.github.com>
2020-08-24 20:34:32 +0200

    build changes

---
+++ build/precache-manifest.da5b09c8a8227d04209ac955e0fe79d7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3b8897bc00b7c52e058338cdf09e613",
+    "url": "./index.html"
+  },
+  {
+    "revision": "30d6abe11d5d99b5dda9",
+    "url": "./static/js/2.3d70f15f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.3d70f15f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "955bd6321c8dad3d17ca",
+    "url": "./static/js/main.da7ee56c.chunk.js"
+  },
+  {
+    "revision": "6253c495a8599138515e",
+    "url": "./static/js/runtime-main.77729539.js"
+  },
+  {
+    "revision": "1cd294a771f26752bbb8d8d5210f6412",
+    "url": "./static/media/Nunito-Bold.1cd294a7.ttf"
+  },
+  {
+    "revision": "c1678b46f7dd3f50ceac94ed4e0ad01a",
+    "url": "./static/media/SourceSansPro-Regular.c1678b46.ttf"
+  },
+  {
+    "revision": "3e80d8bc986877c08a33d80e24dac71b",
+    "url": "./static/media/emoji.3e80d8bc.jpg"
+  },
+  {
+    "revision": "93603265cfad6c4e988c973dda25c018",
+    "url": "./static/media/katowice.93603265.jpg"
+  },
+  {
+    "revision": "11817d2858289dfc3ae6ef7fd5a3c955",
+    "url": "./static/media/logo-website-yellow.11817d28.png"
+  }
+]);
